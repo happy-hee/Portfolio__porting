e00008d8cff37ba1e675efaa49413f4a10def3cd
Happy Hee <56466589+happy-hee@users.noreply.github.com>
2024-10-31 02:46:30 +0000

    모바일 기기 접속시 학습창 구분 처리

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
+// 모바일 접속여부 확인
+let isMobile = navigator.userAgent.match(/Android|Mobile|iP(hone|od|ad)|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/);
+
 /**
  * 학습창 열기
  */
 function openStudy() {
-  // 팝업 크기
-  let popupW = "1280"
-  let popupH = "724"
-  // 화면 중앙 정렬
-  let left = Math.ceil((window.screen.width - popupW)/2);
-  let top = Math.ceil((window.screen.height - popupH)/2);
-  // 팝업 띄우기
-  let popup = window.open("./contents/01/001.html", "학습창", `width=${popupW}px, height=${popupH}px, left=${left}, top=${top}`);
+  // PC 접속일 경우
+  if(!isMobile){
+    // 팝업 크기
+    let popupW = "1280"
+    let popupH = "724"
+    // 화면 중앙 정렬
+    let left = Math.ceil((window.screen.width - popupW)/2);
+    let top = Math.ceil((window.screen.height - popupH)/2);
+    // 팝업 띄우기
+    let popup = window.open("./contents/01/001.html", "학습창", `width=${popupW}px, height=${popupH}px, left=${left}, top=${top}`);
+  } else {
+  // 모바일 접속일 경우
+    window.open("./mcontents/index.html", "_blank");
+  }
 }
 
 // (접근성) 과정이미지 엔터시 학습창 열기
